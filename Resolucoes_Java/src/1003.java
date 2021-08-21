601f8a725d979ed95387107943db11602ae64ca4
unknown <renatonogueirasenai@gmail.com>
2021-08-21 09:52:37 -0300

    up

---
+++ Resolucoes_Java/src/1003.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+//Leia dois valores inteiros. A seguir, calcule o produto entre estes dois valores e atribua esta operação à variável PROD. A seguir mostre a variável PROD com mensagem correspondente.   
+//
+//Entrada
+//O arquivo de entrada contém 2 valores inteiros.
+//
+//Saída
+//Imprima a mensagem "PROD" e a variável PROD conforme exemplo abaixo, com um espaço em branco antes e depois da igualdade. Não esqueça de imprimir o fim de linha após o produto, caso contrário seu programa apresentará a mensagem: “Presentation Error”.
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int a;
+		int b;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		System.out.printf("SOMA = %d\n", (a + b));
+
+		sc.close();
+
+	}
+
+}
